b39dd124b98939d553c4056438d456995aa92120
Adam Kiss <iam@adamkiss.com>
2021-06-29 20:15:08 +0200

    More complex (cased) naming

--- site/plugins/plugin/index.php
+++ site/plugins/plugin/index.php
@@ -2,7 +2,7 @@
 
 Kirby\Cms\App::plugin('adam/plainkit-test', [
 	'blueprints' => [
-		'fields/demo' => __DIR__ . '/demoField.yml',
+		'fields/demoField' => __DIR__ . '/demoField.yml',
 		'pages/home' => __DIR__ . '/homePage.yml'
 	]
 ]);
